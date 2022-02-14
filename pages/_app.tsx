3aab2e8408152b4d74f4d411869ee073ca79a0a6
AJ <ancheetah@users.noreply.github.com>
2022-02-14 17:27:20 -0500

    wrap with isBrowser

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,13 @@ import { builder, Builder } from '@builder.io/react'
 import builderConfig from '@config/builder'
 builder.init(builderConfig.apiKey)
 
-Builder.set({ 
-  customInsertMenu: true,
-  hideABTab: true, 
-})
+if (Builder.isBrowser) {
+  Builder.set({ 
+    customInsertMenu: true,
+    hideABTab: true, 
+  })
+  console.log('Settings changed!')
+}
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
