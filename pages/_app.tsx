ccbd4f29984c04bc694bf2858fa23ca919189ba8
AJ <ancheetah@users.noreply.github.com>
2022-02-14 17:40:12 -0500

    move useEffect before return

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,16 @@ import { builder, Builder } from '@builder.io/react'
 import builderConfig from '@config/builder'
 builder.init(builderConfig.apiKey)
 
-useEffect( () => {
-  if (Builder.isBrowser) {
-    Builder.set({ 
-      customInsertMenu: true,
-      hideABTab: true, 
-    })
-    console.log('Settings changed!')
-  }
-}, [])
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect( () => {
+    if (Builder.isBrowser) {
+      Builder.set({ 
+        customInsertMenu: true,
+        hideABTab: true, 
+      })
+      console.log('Settings changed!')
+    }
+  }, [])
   return <Component {...pageProps} />
 }
