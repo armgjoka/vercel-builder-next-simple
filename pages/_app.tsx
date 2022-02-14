3a822564fbb68a9bb1228b0392e9cc341ef6d3fb
AJ <ancheetah@users.noreply.github.com>
2022-02-14 17:35:28 -0500

    wrap with useEffect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
 import type { AppProps } from 'next/app'
-
+import { useEffect } from 'react'
 import { builder, Builder } from '@builder.io/react'
 import builderConfig from '@config/builder'
 builder.init(builderConfig.apiKey)
 
-if (Builder.isBrowser) {
-  Builder.set({ 
-    customInsertMenu: true,
-    hideABTab: true, 
-  })
-  console.log('Settings changed!')
-}
+useEffect( () => {
+  if (Builder.isBrowser) {
+    Builder.set({ 
+      customInsertMenu: true,
+      hideABTab: true, 
+    })
+    console.log('Settings changed!')
+  }
+}, [])
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
