e7295a26d0192fd98241d85a3a4e31f196c3f338
armela <armela@builder.io>
2022-03-22 16:58:58 +0100

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,27 @@
 import type { AppProps } from 'next/app'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { builder, Builder } from '@builder.io/react'
 import builderConfig from '@config/builder'
 builder.init(builderConfig.apiKey)
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [inUseEffect, setinUseEffect] = useState('default');
+  
+  useEffect(() => {
+    setinUseEffect('use effect');
     if (Builder.isBrowser) {
-      console.log('Set editor options');
+      setinUseEffect('browser');
+      console.log('Set editor options: ', inUseEffect);
       Builder.set({ 
         customInsertMenu: true,
         hideABTab: true, 
       })
-      console.log('Settings changed 2!')
     }
-  return <Component {...pageProps} />
+  }, [inUseEffect])
+
+  return  <div>
+    <p>Component rendered {inUseEffect}</p>
+    <Component {...pageProps} />
+    </div>
 }
