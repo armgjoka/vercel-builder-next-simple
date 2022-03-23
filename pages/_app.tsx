97e9f963995965f7157582aa0d6cd4d5ad2d5e4b
armela <armela@builder.io>
2022-03-23 08:40:47 +0100

    remove check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,22 +6,16 @@ builder.init(builderConfig.apiKey)
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [inUseEffect, setinUseEffect] = useState('default');
-  
+
   useEffect(() => {
-    setinUseEffect('use effect');
-    if (Builder.isBrowser) {
-      setinUseEffect('browser');
-      console.log('Set editor options: ', inUseEffect);
       Builder.set({ 
         customInsertMenu: true,
         hideABTab: true, 
       })
-    }
-  }, [inUseEffect])
+  }, [])
 
   return  <div>
-    <p>Component rendered {inUseEffect}</p>
+    <p>Component rendered</p>
     <Component {...pageProps} />
     </div>
 }
