67de58b2ea467c68a0a6e011f172de4e54c77eee
armela <armela@builder.io>
2022-03-23 09:51:05 +0100

    test editor.settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,18 @@ builder.init(builderConfig.apiKey)
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const builderSettings = JSON.stringify(Builder.settings, null, 2);
-  Builder.set({ 
-    customInsertMenu: true,
-    hideABTab: true, 
+
+  const [settings, setSettings] = useState('');
+  
+  Builder.settingsChange.subscribe(r => setSettings(JSON.stringify(r, null, 2)))
+  
+  Builder.register('editor.settings', {
+    hideABTab: true,
   })
+  
 
   return  <div>
-    <p>Component rendered {builderSettings}</p>
+    <p>Component rendered {settings}</p>
     <Component {...pageProps} />
     </div>
 }
