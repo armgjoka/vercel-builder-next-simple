d13cf3e71a343300959626e16ef34ba1af627a62
armela <armela@builder.io>
2022-04-14 11:41:25 +0200

    try set options

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,25 +7,24 @@ builder.init(builderConfig.apiKey)
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 
-/*   useEffect( () => {
+ useEffect(() => {
     if (Builder.isBrowser) {
-      console.log('Set editor options');
       Builder.set({ 
         customInsertMenu: true,
         hideABTab: true, 
       })
       console.log('Settings changed 2!')
     }
-  }, []) */
+  }, []) 
 
-  useEffect(() => {
-    if (Builder.isBrowser) {
-        Builder.register('editor.settings', {
-          hideABTab: true,
-          customInsertMenu: true,
-        })
-    }
-  }, []);
+ // useEffect(() => {
+ //   if (Builder.isBrowser) {
+ //       Builder.register('editor.settings', {
+ //         hideABTab: true,
+ //         customInsertMenu: true,
+ //       })
+  //  }
+  // }, []); 
 
   return  <Component {...pageProps} />
 }
